6fddad25d97a71b52c5235d60771c2ff6ec14e00
Justice08-byte <4354153@myuwc.ac.za>
2026-03-04 14:16:51 +0200

    Update TimingExperiment.java

--- TimingExperiment.java
+++ TimingExperiment.java
@@ -1,1 +1,14 @@
+//Data Generation 
+
+public class KeyValue{
+  String key;
+  String value;
+  pulic KeyValue(String key, String value){
+    this.key = key;
+    this.value = value;
+  }
+}
+
+// Data shuffling and generation
+
 
