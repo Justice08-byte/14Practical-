e5a6fb51408ad3ee807e3bf72d3c5bc6a94983c8
Justice08-byte <4354153@myuwc.ac.za>
2026-03-04 14:30:32 +0200

    Update TimingExperiment.java

--- TimingExperiment.java
+++ TimingExperiment.java
@@ -10,5 +10,19 @@ public class KeyValue{
 }
 
 // Data shuffling and generation
+public class dataGenerate{
+  public static KeyValue[] generateData(int N){
+    KeyValue[] data = new KeyValue[N];
+    for(int i = 0; i<N; i++){
+      data[i] = new KeyValue(String.valueOf(i+1),"");
+    }
+    List<KeyValue>list = Arrays.asList(data);
+    Collections.shuffle(List);
+    for(int i = 0; i<N; i++){
+      list.get(i).value = String.valueOf(i+1);
+    }
+    return list.toArray(new KeyValue[0];
+  }
+}
 
 
